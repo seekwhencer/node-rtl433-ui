7747698afc9f5189f3a701da485112feceba7f2b
seekwhencer <matthias.kallenbach@gmail.com>
2023-04-24 23:22:07 +0200

    docker compose for github package

--- build.sh
+++ build.sh
@@ -8,25 +8,18 @@
 loadConfig() {
     DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     export $(egrep -v '^#' "${DIR}/.env" | xargs)
+
+    export TAG_LATEST="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:latest"
+    export TAG_VERSION="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
+
 }
 
 # build the docker image
 build() {
   env
-  local TAG_LATEST="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:latest"
-  local TAG_VERSION="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
-
   #docker build . -t ${TAG_LATEST} -t ${TAG_VERSION} -f ./server/Dockerfile
 
-  docker buildx build . --builder=${DOCKER_BUILDX_NAME} --platform=${DOCKER_BUILDX_PLATFORM} -t ${TAG_LATEST} -t ${TAG_VERSION} -f ./server/Dockerfile ./server/Dockerfile
-}
-
-push() {
-  local TAG_LATEST="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:latest"
-  local TAG_VERSION="${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
-
-  docker push ${TAG_LATEST}
-  docker push ${TAG_VERSION}
+  docker buildx build . --builder=${DOCKER_BUILDX_NAME} --platform=${DOCKER_BUILDX_PLATFORM} --push -t ${TAG_LATEST} -t ${TAG_VERSION} -f ./server/Dockerfile
 }
 
 #
