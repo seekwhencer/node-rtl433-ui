9f826fe408b459f4e3510a5ddb915662c9abfc5b
seekwhencer <matthias.kallenbach@gmail.com>
2023-04-27 12:40:23 +0200

    docker build

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -e
 
-#
-# this copies the frontend production bundle in place for the server
-#
-# server/config/frontend is the delivery folder for the
-# frontend statics in production
-#
-mkdir -p /raspiscan/server/config/frontend
-curl https://raw.githubusercontent.com/seekwhencer/node-rtl433-ui/frontend-production/index.html -s -o /raspiscan/server/config/frontend/index.html
-curl https://raw.githubusercontent.com/seekwhencer/node-rtl433-ui/frontend-production/app.js -s -o /raspiscan/server/config/frontend/app.js
-
 # Run command with node if the first argument contains a "-" or is not a system command. The last
 # part inside the "{}" is a workaround for the following bug in ash/dash:
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
